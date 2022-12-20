5dfd39ab27e9999c986d641ce6815c3394ed9639
GluboInternational <121129494+GluboInternational@users.noreply.github.com>
2022-12-20 23:51:18 +0100

    .

---
+++ script.js
@@ -0,0 +1,25 @@
+// Get the button that will open the modal
+const openModalButton = document.getElementById("openModalButton");
+    
+// Get the modal
+const imageModal = document.getElementById("imageModal");
+
+// Get the close button
+const closeModalButton = document.getElementById("closeModalButton");
+
+// When the user clicks the button, open the modal
+openModalButton.addEventListener("click", function() {
+    imageModal.style.display = "block";
+});
+
+// When the user clicks the close button, close the modal
+closeModalButton.addEventListener("click", function() {
+    imageModal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener("click", function(event) {
+    if (event.target == imageModal) {
+    imageModal.style.display = "none";
+    }
+});
